0699081a875d3986994045c62a7482bb09f61565
amanhaidry <amanhaidry9480@gmail.com>
2025-04-30 23:31:32 +0530

    fix: update icon paths in PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: "#1e2331",
         icons: [
           {
-            src: "/logo.png",
+            src: "public/hoobank.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/hoobank.png",
+            src: "public/hoobank.png",
             sizes: "512x512",
             type: "image/png",
           },
